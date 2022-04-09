b3da1258afb4f4c941cdd0e913dd1296faf6d13f
Wellington Okabayashi <dametchanx@gmail.com>
2022-04-09 22:45:53 +0900

    upload/api

---
+++ api/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.listen(5500, () => console.log('Rodando na porta 5500'))
+
+app.use(cors())
+
+app.use(express.json())
+
+let users = [
+  {
+    id: 1,
+    name: 'ADMIN',
+    email: 'seuemail@seuemail.com',
+    senha: '123123'
+  }
+]
+
+app.route('/api').get((req, res) =>
+  res.json({
+    users
+  })
+)
+
+app.route('/api/:id').get((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User not found!')
+  }
+
+  res.json(user)
+})
+
+app.route('/api').post((req, res) => {
+  const lastId = users[users.length - 1].id
+  users.push({
+    id: lastId + 1,
+    name: req.body.name,
+    email: req.body.email,
+    senha: req.body.senha
+  })
+  res.json('Saved user')
+})
+
+app.route('/api/:id').put((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  const updatedUser = {
+    ...user,
+
+    senha: req.body.senha
+  }
+
+  users = users.map(user => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser
+    }
+    return user
+  })
+
+  res.json('Updated user')
+})
+
+app.route('/api/:id').delete((req, res) => {
+  const userId = req.params.id
+
+  users = users.filter(user => Number(user.id) !== Number(userId))
+
+  res.json('Deleted User')
+})
--- js/login.js
+++ js/login.js
@@ -42,8 +42,11 @@ function logar() {
       }
       let token = Math.random().toString(16).substr(2)
       localStorage.setItem('token', token)
-      let userLogado = listaUser[0]
-      localStorage.setItem('userLogado', JSON.stringify(listaUser[0].name))
+      let login = listaUser[0]
+      let senha = listaUser[0]
+
+      localStorage.setItem('senha', JSON.stringify(listaUser[0].senha))
+      localStorage.setItem('login', JSON.stringify(listaUser[0].name))
     } else {
       msgerror.setAttribute('style', 'display:block')
       msgerror.innerHTML = 'Email ou Senha incorretos'
@@ -59,11 +62,36 @@ function sair() {
   window.location.href = '../index.html'
 }
 function loged() {
-  let userLog = JSON.parse(localStorage.getItem('userLogado'))
-
+  let userLog = JSON.parse(localStorage.getItem('login'))
+  let senhaLog = JSON.parse(localStorage.getItem('senha'))
+  console.log(userLog)
   let usuario = document.querySelector('#usuario')
+  let dash = document.querySelector('.dash-box')
 
   usuario.innerHTML = `${userLog}`
+
+  dash.innerHTML = `
+  <div class="dash-perfil">
+  <div class="dash-log" >
+  <h4>Logado como : </h4>
+  <h2>${userLog}</h2>
+  <h4>Digite sua nova senha</h4>
+          <div class="campos">
+          <input
+            id="senha"
+            class="senha"
+            type="text"
+            placeholder="${senhaLog}"
+            required
+          />
+          <div class="update">
+          <button onclick="updateSenha()">Mudar Senha</button>
+        </div>
+  </div>
+  
+  
+  
+  `
 }
 loged()
 function check() {
@@ -73,3 +101,42 @@ function check() {
   }
 }
 check()
+
+//===========server==========//
+const url = 'http://localhost:5500/api'
+
+function getUsers() {
+  axios
+    .get(url)
+    .then(response => {
+      const data = JSON.stringify(response.data)
+      console.log(data)
+    })
+
+    .catch(error => console.log(error))
+}
+
+function getUser() {
+  axios
+    .get(`${url}/1`)
+
+    .then(response => {
+      const data = response.data
+
+      console.log(data)
+    })
+    .catch(error => console.log(error))
+}
+
+function updateSenha() {
+  let senha = document.querySelector('#senha').value
+
+  let newSenha = senha
+  console.log(newSenha)
+  axios
+    .put(`${url}/1`, newSenha)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => console.error(error))
+}
